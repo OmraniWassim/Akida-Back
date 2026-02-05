bcb43a1eed9e3cff30da79f09edd8f1c22e503f6
OmraniWassim <omraniwassim33@gmail.com>
2026-02-05 22:11:27 +0100

    Update ProductAttribute.java

--- src/main/java/com/akida/ecommerce/models/ProductAttribute.java
+++ src/main/java/com/akida/ecommerce/models/ProductAttribute.java
@@ -11,8 +11,8 @@ import lombok.Setter;
 @Table(name = "product_attributes")
 public class ProductAttribute extends BasicEntity{
     @Id
-    @SequenceGenerator(name="product_attributes_sequence",sequenceName ="appuser_sequence",allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "appuser_sequence")
+    @SequenceGenerator(name="product_attributes_sequence",sequenceName ="product_attributes_sequence",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "product_attributes_sequence")
     private Long id;
 
     @Column(nullable = false)
